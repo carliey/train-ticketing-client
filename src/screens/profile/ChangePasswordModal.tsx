da5e96fb31a70ed896087b3e4425b86be95f272c
carliey <mdcarliey@gmail.com>
2023-05-09 17:39:23 +0100

    change password form

--- src/screens/profile/ChangePasswordModal.tsx
+++ src/screens/profile/ChangePasswordModal.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Dialog, { DialogProps } from "@mui/material/Dialog";
@@ -6,12 +6,13 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import Switch from "@mui/material/Switch";
+import { TextField } from "@mui/material";
+
+type FormValues = {
+  oldPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+};
 
 type Props = {
   open: boolean;
@@ -19,32 +20,81 @@ type Props = {
 };
 
 export default function ChangePasswordModal(props: Props) {
+  const [values, setValues] = useState<FormValues>({
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.currentTarget;
+    setValues((prev: any) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async () => {
+    console.log("data", values);
+  };
+
   return (
-    <React.Fragment>
-      <Dialog
-        fullWidth={true}
-        maxWidth={"sm"}
-        open={props.open}
-        onClose={props.handleClose}
-      >
-        <DialogTitle>Change Password</DialogTitle>
-        <DialogContent>
-          <DialogContentText>Change password here</DialogContentText>
-          <Box
-            noValidate
-            component="form"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              m: "auto",
-              width: "fit-content",
-            }}
-          ></Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={props.handleClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+    <Dialog
+      fullWidth={true}
+      maxWidth={"sm"}
+      open={props.open}
+      onClose={props.handleClose}
+    >
+      <DialogTitle>Change Password</DialogTitle>
+      <DialogContent>
+        <Box
+          noValidate
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+          }}
+        >
+          <TextField
+            fullWidth
+            variant="standard"
+            label="Old password"
+            name="oldPassword"
+            value={values.oldPassword}
+            onChange={handleChange}
+            type="password"
+          />
+          <TextField
+            variant="standard"
+            label="New Password"
+            name="newPassword"
+            value={values.newPassword}
+            onChange={handleChange}
+            type="password"
+          />
+          <TextField
+            variant="standard"
+            label="Confirm Password"
+            name="confirmPassword"
+            value={values.confirmPassword}
+            onChange={handleChange}
+            type="password"
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" size="small" onClick={handleSubmit}>
+          Submit
+        </Button>
+        <Button
+          size="small"
+          color="error"
+          variant="contained"
+          onClick={props.handleClose}
+        >
+          Cancel
+        </Button>{" "}
+      </DialogActions>
+    </Dialog>
   );
 }
